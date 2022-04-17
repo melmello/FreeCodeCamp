bfa04706c41c993e00efd10ef2a4a41f873e8956
Giulio Melloni <g.melloni@switchup.it>
2022-04-17 16:01:57 +0200

    feat(ml): ended challenges

---
+++ Machine Learning with Python/conda-env.sh
@@ -0,0 +1,5 @@
+conda install -c apple -y tensorflow-deps
+pip install --yes tensorflow-macos -y
+pip install --yes tensorflow-metal
+conda install -c conda-forge -y pandas jupyter
+pip install --yes tensorflow_datasets
